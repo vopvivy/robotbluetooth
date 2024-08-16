e5734dd2f4ecfd0f346117044c1a1eb9497bba81
vopvivy <50803878+vopvivy@users.noreply.github.com>
2024-08-16 20:48:22 +0300

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,8 +17,112 @@ bluetooth.onBluetoothDisconnected(function () {
         `)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-	
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.forward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.forward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        speedMotorValue += 10
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        speedMotorValue += -10
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    }
 })
+let speedMotorValue = 0
+speedMotorValue = 10
+MotorDriver.MotorStop(Motor.A)
+MotorDriver.MotorStop(Motor.B)
 basic.showLeds(`
     # . . # #
     # . . # #
@@ -26,5 +130,3 @@ basic.showLeds(`
     # . # . .
     # # . . .
     `)
-MotorDriver.MotorStop(Motor.A)
-MotorDriver.MotorStop(Motor.B)
