98d107fdfa31321360dfb6d4c260bfb93450f1a0
vopvivy <50803878+vopvivy@users.noreply.github.com>
2024-08-25 18:09:39 +0300

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ bluetooth.onBluetoothDisconnected(function () {
     MotorDriver.MotorStop(Motor.B)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         MotorDriver.MotorRun(Motor.A, Dir.forward, speedMotorValue)
         MotorDriver.MotorRun(Motor.B, Dir.forward, speedMotorValue)
         basic.showLeds(`
@@ -29,80 +29,9 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . # . .
             . . # . .
             `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
-        MotorDriver.MotorStop(Motor.A)
-        MotorDriver.MotorStop(Motor.B)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . . # . .
-            . . . # .
-            . # # # .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
-        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
-        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
-        MotorDriver.MotorStop(Motor.A)
-        MotorDriver.MotorStop(Motor.B)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . . # . .
-            . . . # .
-            . # # # .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
-        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
-        MotorDriver.MotorRun(Motor.B, Dir.forward, speedMotorValue)
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
-        MotorDriver.MotorStop(Motor.A)
-        MotorDriver.MotorStop(Motor.B)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . . # . .
-            . . . # .
-            . # # # .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
-        MotorDriver.MotorRun(Motor.A, Dir.forward, speedMotorValue)
-        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
-        MotorDriver.MotorStop(Motor.A)
-        MotorDriver.MotorStop(Motor.B)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . . # . .
-            . . . # .
-            . # # # .
-            `)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
-        speedMotorValue += Math.constrain(speedMotorValue + 10, 10, 90)
-        basic.showNumber(speedMotorValue / 10)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
         basic.showLeds(`
             . # # # .
             . # . . .
@@ -111,9 +40,84 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # # # .
             `)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
-        speedMotorValue += Math.constrain(speedMotorValue - 10, 10, 90)
-        basic.showNumber(speedMotorValue / 10)
+        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.backward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        MotorDriver.MotorRun(Motor.A, Dir.forward, speedMotorValue)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, speedMotorValue)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        speedMotorValue += 10
+        speedMotorValue = Math.constrain(speedMotorValue, 10, 90)
+        basic.clearScreen()
+        basic.showNumber(speedMotorValue / 10)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . . # . .
+            . . . # .
+            . # # # .
+            `)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        speedMotorValue += -10
+        speedMotorValue = Math.constrain(speedMotorValue, 10, 90)
+        basic.clearScreen()
+        basic.showNumber(speedMotorValue / 10)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
         basic.showLeds(`
             . # # # .
             . # . . .
